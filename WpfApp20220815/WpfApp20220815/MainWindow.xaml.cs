3d319a6ca2fb6a33313b13dcc7ca49484bbf7cb8
SoilHeatFlux <s.veroku@gmail.com>
2022-08-23 04:40:10 +0900

    一回目の作成

--- WpfApp20220815/WpfApp20220815/MainWindow.xaml.cs
+++ WpfApp20220815/WpfApp20220815/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,7 @@ namespace WpfApp20220815
     /// </summary>
     public partial class MainWindow : Window
     {
+        
 
         public class Account
         {
@@ -33,26 +36,74 @@ namespace WpfApp20220815
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Account account = new Account
+            Create_Directory(sender,e);
+        }
+
+
+        static int Execute_Command(string command, string arguments = "")
+        {
+            var process = new Process
             {
-                Name = "John Doe",
-                Email = "john@microsoft.com",
-                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+                StartInfo = new ProcessStartInfo(command)
+                {
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                }
             };
-            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-            TextBlock.Text = json;
+            process.Start();
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+
+
+
+        static void Git_Command(string arguments)
+        {
+            if (Execute_Command("git", arguments) != 0)
+            {
+                throw new Exception("git command error");
+            }
+        }
+
+        static void Git_Fllow(string outDir,string gitUrl, string commitId　,String target)
+        {
+            //ディレクトリ作成
+            Directory.CreateDirectory(outDir);
+            //リポジトリ初期化
+            Git_Command($@"-C {outDir} init");
+            //sparsecheckout設定
+            Git_Command($@"-C {outDir} config core.sparsecheckout true");
+            //sparsecheckout init
+            Git_Command($@"-C {outDir} sparse-checkout init");
+            //取得対象をsparse-checkoutに設定する
+            Git_Command($@"-C {outDir} sparse-checkout add {target}");
+            Git_Command($@"-C {outDir} remote add origin {gitUrl}");
+            Git_Command($@"-C {outDir} pull origin {commitId}");
         }
 
         private void Create_Directory(object sender, RoutedEventArgs e)
         {
-
-            string sampleText = SampleTextBox.Text;
-            Directory.CreateDirectory(sampleText);
-            SampleTextBox.Text = "Sample text retrieved and "  + sampleText;
-            
+            string gitUrl = TargetGit.Text;
+            string commitIdAfter = CommitIdAfter.Text;
+            string commitIdBefore = CommitIdBefore.Text;
+            string outDir = OutDir.Text;
+            string target = target1.Text;
+            Git_Fllow(outDir + "/before", gitUrl, commitIdBefore, target);
+            Git_Fllow(outDir + "/after", gitUrl, commitIdAfter, target);
 
         }
 
+        private void TargetGit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        /*        private string GetIndex0()
+       {
+           ListBoxItem lbi = (ListBoxItem)
+               (targetUrlList.ItemContainerGenerator.ContainerFromIndex(0));
+           string hoge = lbi.Content.ToString();
+           return hoge;
+       }*/
 
 
     }
